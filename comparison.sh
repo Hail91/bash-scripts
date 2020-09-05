856631ce1989569fc3692019d3602b21a88ddd24
Hail91 <aarongillies88@gmail.com>
2020-09-05 16:20:19 -0400

    updated comparison script with conditionals

--- comparison.sh
+++ comparison.sh
@@ -3,13 +3,17 @@
 string_a="UNIX"
 string_b="GNU"
 
-echo "Are $string_a and $string_b equal?"
-[ $string_a = $string_b ]
-echo $?
+if [ $string_a != $string_b ]; then
+    echo "The strings are not equal!"
+else
+    echo "The strings are equal!"
+fi
 
 num_a=100
 num_b=100
 
-echo "Us $num_a equal to $num_b?"
-[ $num_a -eq $num_b ]
-echo $?
+if [ $num_a -eq $num_b ]; then
+    echo "The integers are equal!"
+else    
+    echo "The integers are not equal!"
+fi
