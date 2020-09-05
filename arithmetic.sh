e59afae4b5c79b75441ffac55a453877343e9055
Hail91 <aarongillies88@gmail.com>
2020-09-05 17:40:16 -0400

    added a few comments

--- arithmetic.sh
+++ arithmetic.sh
@@ -15,3 +15,12 @@ echo $c
 # Division
 d=$(( 100 / 5 ))
 echo $d 
+
+# Can also use expr command for evaluation
+# Can use 'let' to evaluate an expression and assign to a variable in same line.
+# Can use 'bc' for more complex Mathematical operations,
+# It's commonly used for decimal expressions
+# use 'scale' to output a value to a specified number of digits.
+
+e=$( echo 'scale=2; 8.5 / 2.3' | bc)
+echo $e
