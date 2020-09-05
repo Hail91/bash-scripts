f7d2777b8bd8b9fcdd1328e71b074ba159117c41
Hail91 <aarongillies88@gmail.com>
2020-09-05 15:38:22 -0400

    added simple backup script to backup home directory

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script will generate a backup of a user's home directory to a /tmp/ folder.
+user=$(whoami)
+input=/home/user
+output=/tmp/${user}_home_$(data +%Y -%d_%H%M%S).tar.gz
+
+tar -czf $output $input
+echo "Backup of $input completed, details about the output backup file:"
+ls -l $output
--- welcome.sh
+++ welcome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-greeting="Welcome!"
+# Never name private variables with uppercase characters as this can overwrite reserved internal bash commands!
+greeting="Welcome"
 user=$(whoami)
 day=$(date +%A)
 
-echo "$greeting back $user! Today is $day, have a great week!"
+echo "$greeting back ${user^}! Today is $day, have a great week!"
 echo "Your current Bash shell version is: $BASH_VERSION. Enjoy!"
