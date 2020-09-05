27d96369ceb450a18d06e2e9152c7fdf56c91a26
Hail91 <aarongillies88@gmail.com>
2020-09-05 16:56:30 -0400

    updated script with until loop for practice

--- loops.sh
+++ loops.sh
@@ -7,10 +7,21 @@ for i in 1 2 3 4 5; do
 done
 
 echo "Break"
+
 # Basic while loop
 
 counter=0
 while [ $counter -lt 5 ]; do
     let counter+=1
     echo $counter
+done
+
+# Basic until loop
+
+echo "Break"
+
+until_counter=5
+until [ $until_counter -lt 2 ]; do
+    let until_counter-=1
+    echo $until_counter
 done
