7735495c177549052ea63a956ce81e0765a57da4
Hail91 <aarongillies88@gmail.com>
2020-09-05 17:02:57 -0400

    added basic arithmetic expressions

---
+++ arithmetic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Addition
+a=$(( 10 + 20 ))
+echo $a
+
+# Subtraction
+b=$(( 15 - 5 ))
+echo $b 
+
+# Multiplication
+c=$(( 20 * 5 ))
+echo $c
+
+# Division
+d=$(( 100 / 5 ))
+echo $d 
